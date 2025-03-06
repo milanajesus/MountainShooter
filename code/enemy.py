c4ad486df3911affba4b9ab08bec4196753b2b0f
WESLEYJESUS <wesleyjesus@mpf.mp.br>
2025-03-06 20:03:00 -0300

    Creanting Players and Enemies Moving player and Enemies

--- code/enemy.py
+++ code/enemy.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from Enemy import Enemy
-from Enemy import Enemy
-from Enemy import Enemy
-from Entity import Entity
+from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.entity import Entity
 
 
-class Enemy(Enemy, Enemy, Enemy, Entity):
-    def __init__(self):
-        pass
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
