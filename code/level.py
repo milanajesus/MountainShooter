8d5ed9b81ea098693e967077659147f177a13ad3
WESLEYJESUS <wesleyjesus@mpf.mp.br>
2025-03-11 16:41:54 -0300

    Creating Mediator Pattern. Enemies destruction

--- code/level.py
+++ code/level.py
@@ -10,6 +10,7 @@ from pygame.font import Font
 from code.Const import COLOR_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.entity import Entity
 from code.entityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
 
 
 class Level:
@@ -25,7 +26,7 @@ class Level:
             self.entity_list.append(EntityFactory.get_entity('Player2'))
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
 
-    def run(self):
+    def run(self,):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
@@ -47,6 +48,9 @@ class Level:
             self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
             self.level_text(14, f'entidades: {len(self.entity_list)}', COLOR_WHITE, (10, WIN_HEIGHT - 20))
             pygame.display.flip()
+            # Collisions
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
             pass
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
