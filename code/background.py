3e4e62c95f84f4eab843dc1721c3a55727cd983c
WESLEYJESUS <wesleyjesus@mpf.mp.br>
2025-03-14 11:36:52 -0300

    Hit box Collisions HUD Score

--- code/background.py
+++ code/background.py
@@ -5,7 +5,7 @@ from code.entity import Entity
 
 
 class Background(Entity):
-   def __init__(self, name: str, position: tuple):
+    def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
 
     def move(self, ):
